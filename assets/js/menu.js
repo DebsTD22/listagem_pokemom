bb264978c096890d1832c7dbc77271cc8dcf65bc
DebsTD22 <dominguetidebora@gmail.com>
2024-08-29 19:48:24 -0300

    atualização menu

---
+++ assets/js/menu.js
@@ -0,0 +1,7 @@
+document.getElementById('open_btn').addEventListener('click', function(){
+    document.getElementById('sidebar').classList.toggle('open_sidebar')
+})
+
+document.getElementById('open_btn').addEventListener('click', function(){
+    document.getElementById('open_btn').classList.toggle('open_sidebar')
+})
