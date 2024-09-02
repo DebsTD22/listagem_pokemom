4d4aa21f4b954b6b2ab21be584960f5493df4fa1
DebsTD22 <dominguetidebora@gmail.com>
2024-09-02 13:00:03 -0300

    atualização menu

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,7 +1,12 @@
+
 document.getElementById('open_btn').addEventListener('click', function(){
     document.getElementById('sidebar').classList.toggle('open_sidebar')
 })
 
-document.getElementById('open_btn').addEventListener('click', function(){
-    document.getElementById('open_btn').classList.toggle('open_sidebar')
+document.getElementById('close_btn').addEventListener('click', function(){
+    document.getElementById('sidebar').classList.toggle('open_sidebar')
+})
+
+document.getElementById('side_itens').addEventListener('click', function(){
+    document.getElementById('side_itens').classList.toggle('active')
 })
